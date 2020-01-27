0341824a6009e2024c5e10bacadde85f77d4d6a6
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-26 21:35:44 -0500

    API data fetched

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.scss";
 
-function App() {
-	return <div className='App'></div>;
+class App extends React.Component {
+	state = "";
+
+	componentDidMount() {
+		fetch("http://localhost:5000/api/players")
+			.then(response => response.json())
+			.then(data => this.setState({ data }))
+			.catch(error => console.log("Error in componentDidMount", error));
+	}
+
+	render() {
+		return (
+			<div className='App'>{this.state ? console.log(this.state) : null}</div>
+		);
+	}
 }
 
 export default App;
