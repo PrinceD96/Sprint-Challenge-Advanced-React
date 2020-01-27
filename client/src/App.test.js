e01ab2341bf9dbf4d014b8708407bb498b7b0d07
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-26 23:56:11 -0500

    Added 3 tests.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import App from "./App";
+import DarkMode from "./components/DarkMode";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("Renders without crashing", () => {
+	rtl.render(<App />);
+});
+
+test("localStorage hook works", () => {
+	window.localStorage.removeItem("dark-mode");
+
+	const doc = rtl.render(<Theme />);
+	const toggle = doc.getByLabelText(/theme/i);
+
+	rtl.fireEvent.click(toggle);
+
+	const changedValue = JSON.parse(window.localStorage.getItem("dark-mode"));
+	expect(changedValue).toBe(true);
+});
+
+test("Dark Mode switching works", () => {
+	const doc = rtl.render(<Theme />);
+	const toggle = doc.getByLabelText(/theme/i);
+
+	expect(toggle.classList).toContain("toggle");
+
+	rtl.fireEvent.click(toggle);
+
+	expect(toggle.classList).toContain("toggled");
+
+	expect(toggle).toBeInTheDocument();
 });
